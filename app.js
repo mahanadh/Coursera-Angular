5e7055caf99a1a09a21b4db28c3337d6005d271a
Mahan <mahan2054@gmail.com>
2020-06-24 17:49:43 +0545

    Assignment 3

--- app.js
+++ app.js
@@ -1,95 +1,65 @@
 (function () {
 'use strict';
 
-angular.module('ControllerAsApp', [])
-.controller('ShoppingListController1', ShoppingListController1)
-// .controller('ShoppingListController2', ShoppingListController2)
-.factory('ShoppingListFactory', ShoppingListFactory);
+angular.module('MenuCategoriesApp', [])
+.controller('MenuCategoriesController', MenuCategoriesController)
+.service('MenuCategoriesService', MenuCategoriesService)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
 
-// LIST #1 - controller
-ShoppingListController1.$inject = ['ShoppingListFactory'];
-function ShoppingListController1(ShoppingListFactory) {
-  var list1 = this;
 
-  // Use factory to create new shopping list service
-  var shoppingList = ShoppingListFactory();
+MenuCategoriesController.$inject = ['MenuCategoriesService'];
+function MenuCategoriesController(MenuCategoriesService) {
+  var menu = this;
 
-  list1.items = shoppingList.getItems();
+  var promise = MenuCategoriesService.getMenuCategories();
 
+  promise.then(function (response) {
+    menu.categories = response.data;
+  })
+  .catch(function (error) {
+    console.log("Something went terribly wrong.");
+  });
+
+  menu.logMenuItems = function (shortName) {
+    var promise = MenuCategoriesService.getMenuForCategory(shortName);
+
+    promise.then(function (response) {
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  };
 
 }
 
 
-// // LIST #2 - controller
-// ShoppingListController2.$inject = ['ShoppingListFactory'];
-// function ShoppingListController2(ShoppingListFactory) {
-//   var list2 = this;
-//
-//   // Use factory to create new shopping list service
-//   var shoppingList = ShoppingListFactory(3);
-//
-//   list2.items = shoppingList.getItems();
-//
-//   list2.itemName = "";
-//   list2.itemQuantity = "";
-//
-//   list2.addItem = function () {
-//     try {
-//       shoppingList.addItem(list2.itemName, list2.itemQuantity);
-//     } catch (error) {
-//       list2.errorMessage = error.message;
-//     }
-//
-//   }
-//
-//   list2.removeItem = function (itemIndex) {
-//     shoppingList.removeItem(itemIndex);
-//   };
-// }
-
-
-// If not specified, maxItems assumed unlimited
-function ShoppingListService(maxItems) {
+MenuCategoriesService.$inject = ['$http', 'ApiBasePath'];
+function MenuCategoriesService($http, ApiBasePath) {
   var service = this;
 
-  // List of shopping items
-  var items = [];
+  service.getMenuCategories = function () {
+    var response = $http({
+      method: "GET",
+      url: (ApiBasePath + "/categories.json")
+    });
 
-    var item1 = {
-      name: "Milk",
-      quantity: "10"
-    };
-    var item2 = {
-      name: "Donuts",
-      quantity: "10"
-    };
-    var item2 = {
-      name: "Cookies",
-      quantity: "10"
-    };
-    var item4 = {
-      name: "Chocolate",
-      quantity: "10"
-    };
-    var item5 = {
-      name: "Peanut Butter",
-      quantity: "10"
-    };
-
-    items.push(item1,item2,item3,item4,item5);
-
-  service.getItems = function () {
-    return items;
-  };
-}
-
-
-function ShoppingListFactory() {
-  var factory = function (maxItems) {
-    return new ShoppingListService(maxItems);
+    return response;
   };
 
-  return factory;
+
+  service.getMenuForCategory = function (shortName) {
+    var response = $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json"),
+      params: {
+        category: shortName
+      }
+    });
+
+    return response;
+  };
+
 }
 
 })();
---
+++ module3-assign/app.js
@@ -0,0 +1,60 @@
+var app = angular.module('NarrowItDownApp', []);
+
+app.controller('NarrowItDownController', ['$scope', 'MenuSearchService', function($scope, MenuSearchService) {
+    $scope.searchTerm = 'soup';
+    $scope.search = function(searchTerm) {
+      console.log('Searching for ' + searchTerm);
+      MenuSearchService.getMatchedMenuItems(searchTerm).then(function(foundItems) {
+        $scope.results = foundItems;
+      });
+    };
+
+    $scope.search($scope.searchTerm);
+}]);
+
+app.service('MenuSearchService', ['$http', function($http) {
+    var service = this;
+    service.getMatchedMenuItems = function(searchTerm) {
+        searchTerm = searchTerm.toLowerCase();
+        return $http({
+                method: 'GET',
+                url: 'https://davids-restaurant.herokuapp.com/menu_items.json'
+            })
+            .then(function(response) {
+                var result = response.data.menu_items;
+                console.log('Result: ' + result.length);
+                var foundItems = [];
+                result.forEach(function(obj) {
+                    if (searchTerm != '') {
+                        if ((obj.name.indexOf(searchTerm) != -1) || (obj.description.indexOf(searchTerm) != -1)) {
+                            foundItems.push(obj);
+                        }
+                    }
+                })
+                console.log('Found Items: ' + foundItems.length)
+                return foundItems;
+
+            }, function(error) {
+                console.log('Error from $http' + error);
+            });
+    }
+}]);
+
+app.directive('foundItems', function() {
+    var ddo = {
+      restrict: 'E', //Since this is a directive and not a component, you need to define which type (E = Element since you're using <found-items>)
+      templateUrl: 'directive.html',
+      scope: {
+          items: '=' // This is two-way binding which you'll need for the array
+      },
+      controller: function ($scope) {
+        $scope.remove = function (item) {
+          var index = $scope.items.indexOf(item);
+          if(index >= 0) {
+            $scope.items.splice(index, 1);
+          }
+        }
+      }
+    }
+    return ddo;
+});
